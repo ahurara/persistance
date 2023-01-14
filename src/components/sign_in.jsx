df2bf04425ea91147612a239c8fb98ad4bf12abe
Muhammad Amjad <muhammadamjad00@gmail.com>
2023-01-14 21:43:33 +0500

    Amjad

--- src/components/sign_in.jsx
+++ src/components/sign_in.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React,{useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Sign_in =()=>
 {
+    const [email, setEmail] = useState('');
+    const [password,setPassword]= useState('');
+
+    const history = useNavigate();
+
+    const loginUser=async(e)=>{
+        e.preventDefault();
+
+        const res=await fetch("/studentLogin",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email,password
+              })
+        })
+
+        const data=res.json();
+        if(res.status===400||!data){
+            window.alert('Invalid Credential')
+
+        }
+        else{
+            window.alert('login Successfully')
+            history("/")
+        }
+    }
+
+
+
     return(
         <>
-            <form>
+            <form method="POST" id="register-form">
         <label className="mt-5">Username/Email</label>
         <br></br>
         <div class="input-group mt-2 w-50 ">
-        <input type="text" class="form-control" placeholder="Username" aria-label="Username" name="uname"></input>
+        <input type="email" class="form-control" placeholder="Enter Email" aria-label="Username" name="email"
+        value={email} 
+        onChange={(e)=>setEmail(e.target.value)}
+        ></input>
         </div>
         <label className="mt-5">Password</label>
         <br></br>
         <div class="input-group mt-2 w-50 ">
-        <input type="password" class="form-control"  aria-label="Username" name="password"></input>
+        <input type="password" class="form-control"  aria-label="Username" name="password"
+        value={password} 
+        onChange={(e)=>setPassword(e.target.value)}
+        ></input>
         </div>
-        <button className="btn btn-custom mt-4 mb-4 btn-lg rounded-4" style={{backgroundColor:'#9EEA11',color:'white'}}>Sign in</button>
+        <button className="btn btn-custom mt-4 mb-4 btn-lg rounded-4" style={{backgroundColor:'#9EEA11',color:'white'}}
+        type='submit' name='signup' id='signup' onClick={loginUser}
+        >Sign in</button>
        </form>
         </>
     )
